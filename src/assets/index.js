8474f21f27084734e72b2e90e0ef8172be07e472
sahils38 <sahilsaras38@gmail.com>
2025-02-14 23:38:52 +0530

    Updated Browserslist DB

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,9 +3,7 @@ import bannerImg from "./images/bannerImg.png";
 import projectOne from "./images/projects/projectOne.png";
 import projectTwo from "./images/projects/projectTwo.png";
 import projectThree from "./images/projects/projectThree.png";
-import testimonialOne from "./images/testmonial/testimonialOne.png";
-import testimonialTwo from "./images/testmonial/testimonialTwo.png";
-import quote from "./images/testmonial/quote.png";
+
 import contactImg from "./images/contact/contactImg.png";
 
 export {
